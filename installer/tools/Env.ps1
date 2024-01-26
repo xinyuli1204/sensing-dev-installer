195b4a05ba7ff77e989cc7ee5775423476a65c1f
Sonali <139267932+fixstars-sonali@users.noreply.github.com>
2024-01-26 12:33:12 -0800

    Remove update of python path (#57)

--- installer/tools/Env.ps1
+++ installer/tools/Env.ps1
@@ -21,18 +21,16 @@ File Name      : Env.ps1
 Prerequisite   : PowerShell V3
 #>
 param(
-    [string]$installPath= (Split-Path $PSScriptRoot -Parent)    
+    [string]$installPath = (Split-Path $PSScriptRoot -Parent)    
 )
 
 # Define the paths you want to add
 $newPath = "${installPath}\bin"
-$newPythonPath = "${installPath}\lib\site-packages"
 
 Write-Output "installPath : $installPath"
 
-# Get current PATH and PYTHONPATH
+# Get current PATH 
 $currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
-$currentPythonPath = [Environment]::GetEnvironmentVariable("PYTHONPATH", "User")
 
 # Update PATH if the new path is not already in it
 if (-not $currentPath.Contains($newPath)) {
@@ -40,18 +38,6 @@ if (-not $currentPath.Contains($newPath)) {
     [Environment]::SetEnvironmentVariable("Path", $currentPath, "User")
 }
 Write-Verbose "Updated PATH: $currentPath"
-
-# Update PYTHONPATH if the new path is not already in it
-if (-not $currentPythonPath -or (-not $currentPythonPath.Contains($newPythonPath))) {
-    if ($currentPythonPath) {
-        $currentPythonPath += ";$newPythonPath"
-    } else {
-        $currentPythonPath = $newPythonPath
-    }
-    [Environment]::SetEnvironmentVariable("PYTHONPATH", $currentPythonPath, "User")
-}
-Write-Verbose "Updated PYTHONPATH: $currentPythonPath"
-
 
 # Update SENSING_DEV_ROOT if the new path is not already in it
 [Environment]::SetEnvironmentVariable("SENSING_DEV_ROOT", $installPath, "User")
