afe41b08785ef752c171ab2e42812bacf4318727
Fixstars-momoko <momoko.kono@fixstars.com>
2024-07-03 14:27:17 -0700

    updated ion-kit to v1.8.3

--- installer/tools/setup.sh
+++ installer/tools/setup.sh
@@ -30,12 +30,14 @@ unset ion_kit_config
 declare -A ion_kit_config=( # Declare an associative array with default values
     ["v24.05.04"]="v1.8.2"
     ["v24.05.05"]="v1.8.2"
+    ["v24.05.06"]="v1.8.3"
 )
 
 unset gendc_separator_config
 declare -A gendc_separator_config=( # Declare an associative array with default values
     ["v24.05.04"]="v0.2.6"
     ["v24.05.05"]="v0.2.7"
+    ["v24.05.06"]="v0.2.7"
 )
 
 EARLIEST_STABLE_SDK="v24.05.04"
