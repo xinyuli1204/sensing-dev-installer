4c4a06484e324486e1fc821729e59ee6b63fe6b9
Fixstars-momoko <momoko.kono@fixstars.com>
2024-09-27 16:38:04 -0700

    removed tentative path at the end of process

--- installer/tools/pygobject_installer.ps1
+++ installer/tools/pygobject_installer.ps1
@@ -2,94 +2,107 @@ param (
     [string]$CacheDIR
 )
 
-$pipExists = Get-Command pip -ErrorAction SilentlyContinue
+$currentPath = [Environment]::GetEnvironmentVariable("Path", "User")
+Write-Host $currentPath
 
-if ($pipExists){
-    Write-Host "pip found; continue..."
-} else {
-    Write-Error "pip not found; please install it to run this script"
-    exit 1
-}
+try{
+    $pipExists = Get-Command pip -ErrorAction SilentlyContinue
 
-$defaultCacheDIR = Join-Path -Path "$env:TEMP" -ChildPath "PyGObjectCache" 
-
-if (Test-Path $defaultCacheDIR) {
-    Remove-Item -Path $defaultCacheDIR -Force -Recurse
-}
-
-# Validate if the provided CacheDIR parameter is empty
-if (-not $CacheDIR) {
-    Write-Output "Using default cache path: $defaultCacheDIR"
-    $CacheDIR = $defaultCacheDIR
-}
-
-# Define the application ID for pkg-config-lite
-$pkgConfigLiteAppId = "bloodrock.pkg-config-lite"
-
-# Check if pkg-config-lite is already installed using winget
-try {
-    $installedPackage = winget show --id $pkgConfigLiteAppId -e --accept-source-agreements
-    if ($installedPackage) {
-        Write-Output "Uninstalling existing $pkgConfigLiteAppId..."
-        winget uninstall --id $pkgConfigLiteAppId --accept-source-agreements
+    if ($pipExists){
+        Write-Host "pip found; continue..."
+    } else {
+        Write-Error "pip not found; please install it to run this script"
+        exit 1
     }
-} catch {
-    Write-Error "Failed to check/uninstall existing pkg-config-lite: $_"
-    exit 1
+    
+    $defaultCacheDIR = Join-Path -Path "$env:TEMP" -ChildPath "PyGObjectCache" 
+    
+    if (Test-Path $defaultCacheDIR) {
+        Remove-Item -Path $defaultCacheDIR -Force -Recurse
+    }
+    
+    # Validate if the provided CacheDIR parameter is empty
+    if (-not $CacheDIR) {
+        Write-Output "Using default cache path: $defaultCacheDIR"
+        $CacheDIR = $defaultCacheDIR
+    }
+    
+    # Define the application ID for pkg-config-lite
+    $pkgConfigLiteAppId = "bloodrock.pkg-config-lite"
+    
+    # Check if pkg-config-lite is already installed using winget
+    try {
+        $installedPackage = winget show --id $pkgConfigLiteAppId -e --accept-source-agreements
+        if ($installedPackage) {
+            Write-Output "Uninstalling existing $pkgConfigLiteAppId..."
+            winget uninstall --id $pkgConfigLiteAppId --accept-source-agreements
+        }
+    } catch {
+        Write-Error "Failed to check/uninstall existing pkg-config-lite: $_"
+        exit 1
+    }
+    
+    # Install pkg-config-lite using winget
+    Write-Output "Installing $pkgConfigLiteAppId..."
+    try {
+        # Define the directory you want to remove from PATH
+        $pkgconfigDirectory = "$CacheDIR\pkg-config-lite-0.28-1\bin"
+        winget install --accept-source-agreements --accept-package-agreements --id $pkgConfigLiteAppId  --location $CacheDIR
+    } catch {
+        Write-Error "Failed to install pkg-config-lite: $_"
+        exit 1
+    }
+    
+    $tentativePath = "$currentPath;$pkgconfigDirectory"
+    [Environment]::SetEnvironmentVariable("Path", $tentativePath, "User")
+    Write-Output "Added $pkgconfigDirectory to PATH for the current session"
+    
+    # Install gobject-introspection using vcpkg
+    Write-Output "Installing gobject-introspection..."
+    
+    try {
+        Invoke-WebRequest -Uri https://github.com/Sensing-Dev/aravis/releases/download/v0.8.31/PyGObject-1.72.0-dependencies.zip  -OutFile $CacheDIR\dependency.zip
+        Expand-Archive -Path $CacheDIR\dependency.zip -DestinationPath $CacheDIR\dependency
+        Remove-Item -Force $CacheDIR\dependency.zip
+    } catch {
+        Write-Error "Failed to DL/extract gobject-introspection: $_"
+        exit 1
+    }
+    
+    $PkgConfigPath = "$CacheDIR\dependency\pygobject_dependencies\lib\pkgconfig"
+    # Set the PKG_CONFIG_PATH environment variable for the current session
+    Write-Output "Setting PKG_CONFIG_PATH to "$PkgConfigPath" for the current session"
+    $Env:PKG_CONFIG_PATH = $PkgConfigPath
+    
+    # Install pygobject from the Git repository with specific configuration settings
+    Write-Output "Installing pygobject"
+    try {
+        pip install pygobject==3.48.2
+    } catch {
+        Write-Error "Failed to install pygobject: $_"
+        exit 1
+    }
+    
+    # Delete the vcpkg directory recursively
+    Write-Output "Deleting cache directory: "$CacheDIR" ..."
+    try {
+        Remove-Item -Path "$CacheDIR" -Recurse -Force
+    } catch {
+        Write-Error "Failed to remove vcpkg directory: $_"
+        exit 1
+    }
+    
+    # Unset the PKG_CONFIG_PATH environment variable
+    Write-Output "Removing PKG_CONFIG_PATH environment variable"
+    Remove-Item Env:\PKG_CONFIG_PATH
+} finally {
+    [Environment]::SetEnvironmentVariable("Path", $currentPath, "User")
+    Write-Output "Removing $pkgconfigDirectory from PATH"
+
+    Write-Output "Script completed."
 }
 
-# Install pkg-config-lite using winget
-Write-Output "Installing $pkgConfigLiteAppId..."
-try {
-    # Define the directory you want to remove from PATH
-    $pkgconfigDirectory = "$CacheDIR\pkg-config-lite-0.28-1\bin"
-    winget install --accept-source-agreements --accept-package-agreements --id $pkgConfigLiteAppId  --location $CacheDIR
-} catch {
-    Write-Error "Failed to install pkg-config-lite: $_"
-    exit 1
-}
-
-$env:PATH += ";$pkgconfigDirectory"
-Write-Output "Added $pkgconfigDirectory to PATH for the current session"
-
-# Install gobject-introspection using vcpkg
-Write-Output "Installing gobject-introspection..."
-
-try {
-    Invoke-WebRequest -Uri https://github.com/Sensing-Dev/aravis/releases/download/v0.8.31/PyGObject-1.72.0-dependencies.zip  -OutFile $CacheDIR\dependency.zip
-    Expand-Archive -Path $CacheDIR\dependency.zip -DestinationPath $CacheDIR\dependency
-    Remove-Item -Force $CacheDIR\dependency.zip
-} catch {
-    Write-Error "Failed to DL/extract gobject-introspection: $_"
-    exit 1
-}
-
-$PkgConfigPath = "$CacheDIR\dependency\pygobject_dependencies\lib\pkgconfig"
-# Set the PKG_CONFIG_PATH environment variable for the current session
-Write-Output "Setting PKG_CONFIG_PATH to "$PkgConfigPath" for the current session"
-$Env:PKG_CONFIG_PATH = $PkgConfigPath
-
-# Install pygobject from the Git repository with specific configuration settings
-Write-Output "Installing pygobject"
-try {
-    pip install pygobject==3.48.2
-} catch {
-    Write-Error "Failed to install pygobject: $_"
-    exit 1
-}
-
-# Delete the vcpkg directory recursively
-Write-Output "Deleting cache directory: "$CacheDIR" ..."
-try {
-    Remove-Item -Path "$CacheDIR" -Recurse -Force
-} catch {
-    Write-Error "Failed to remove vcpkg directory: $_"
-    exit 1
-}
-
-# Unset the PKG_CONFIG_PATH environment variable
-Write-Output "Removing PKG_CONFIG_PATH environment variable"
-Remove-Item Env:\PKG_CONFIG_PATH
 
 
-Write-Output "Script completed."
+
+
