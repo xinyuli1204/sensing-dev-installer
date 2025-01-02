3b7d75222021192c9e59560c9b85aed4855af606
Fixstars-momoko <momoko.kono@fixstars.com>
2025-01-02 15:35:56 -0800

    fixed python test code

--- installer/testcases/python/gendc_test.py
+++ installer/testcases/python/gendc_test.py
@@ -4,8 +4,7 @@ from gendc_python.genicam import tool as genicam
 
 Mono8 = genicam.pfnc_convert_pixelformat("Mono8")
 
-non_gendc = 'THIS_IS_INVALID_GENDC_BINARY_CONTENT'
-bin_non_gendc = ' '.join(format(ord(x), 'b') for x in non_gendc)
+bin_non_gendc = 'THIS_IS_INVALID_GENDC_BINARY_CONTENT'.encode('ascii')
 
 if __name__ == '__main__':
     try:
